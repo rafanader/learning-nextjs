09cd3f43d770d9793d0878e40e2c52569b191c76
Rafael Nader <81963935+rafanader@users.noreply.github.com>
2022-03-11 09:07:34 -0300

    Second ReactJS class

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,8 @@
+import React from 'react';
 import { Box, Button, Text, TextField, Image} from '@skynexui/components'
-import themeConfig from '../theme.json'
+import { useRouter } from 'next/router';
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+import themeConfig from '../theme.json'
 
 function Title(props){
     const Tag = props.tag || 'h1'
@@ -38,7 +12,6 @@ function Title(props){
             <style jsx>{`
                 ${Tag} {
                     color: ${themeConfig.theme.colors.neutrals['000']};
-                    /*font-size: 24px;*/
                     font-weight: 600;
                 }
             `}
@@ -47,94 +20,168 @@ function Title(props){
     );
 }
 
-export default function HomePage() {
-    const username = 'rafanader';
+export default function Index() {
   
-    return (
-      <>
-        <GlobalStyle />
+  const [usernameText, setUsernameText] = React.useState('rafanader');
+  const [username, setUsername] = React.useState('rafanader');
+  const routerControl = useRouter();
+  const [submitDisabled, setSubmitDisabled] = React.useState(false);
+  
+  function FormSubmitControl(userNameText) {
+    
+    setSubmitDisabled( (userNameText.length < 3) );
+
+    if (userNameText.length > 2) {
+      setUsername(userNameText);
+    }
+  }
+
+  return (
+    <>      
+      <Box
+        styleSheet={{
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          backgroundColor: themeConfig.theme.colors.primary[500],
+          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+        }}
+      >
         <Box
           styleSheet={{
-            display: 'flex', alignItems: 'center', justifyContent: 'center',
-            backgroundColor: themeConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flexDirection: {
+              xs: 'column',
+              sm: 'row',
+            },
+            width: '100%', maxWidth: '700px',
+            borderRadius: '5px', padding: '32px', margin: '16px',
+            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+            backgroundColor: themeConfig.theme.colors.neutrals[700],
           }}
         >
+          {/* Formulário */}
           <Box
+            as="form"
+            onSubmit={function OnSubmitHandler(event) {
+                event.preventDefault();
+                routerControl.push('/chat');
+              }
+          }
             styleSheet={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              flexDirection: {
-                xs: 'column',
-                sm: 'row',
-              },
-              width: '100%', maxWidth: '700px',
-              borderRadius: '5px', padding: '32px', margin: '16px',
-              boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-              backgroundColor: themeConfig.theme.colors.neutrals[700],
+              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            {/* Formulário */}
-            <Box
-              as="form"
-              styleSheet={{
-                display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
-                width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+            <Title tag="h2">Seja bem vindo(a)!</Title>
+            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: themeConfig.theme.colors.neutrals[300] }}>
+              {themeConfig.name}
+            </Text>
+
+            <TextField
+              value={usernameText}
+              onChange={function onChangeHandler(event) {
+                
+                let userNameText = event.target.value;
+                setUsernameText(userNameText);
+                FormSubmitControl(userNameText);
               }}
-            >
-              <Title tag="h2">Seja bem vindo(a)!</Title>
-              <Text variant="body3" styleSheet={{ marginBottom: '32px', color: themeConfig.theme.colors.neutrals[300] }}>
-                {themeConfig.name}
-              </Text>
-  
-              <TextField
-                fullWidth
-                textFieldColors={{
-                  neutral: {
-                    textColor: themeConfig.theme.colors.neutrals[200],
-                    mainColor: themeConfig.theme.colors.neutrals[900],
-                    mainColorHighlight: themeConfig.theme.colors.primary[500],
-                    backgroundColor: themeConfig.theme.colors.neutrals[800],
-                  },
-                }}
-              />
-              <Button
-                type='submit'
-                label='Entrar'
-                fullWidth
-                buttonColors={{
-                  contrastColor: themeConfig.theme.colors.neutrals["000"],
-                  mainColor: themeConfig.theme.colors.primary[500],
-                  mainColorLight: themeConfig.theme.colors.primary[400],
-                  mainColorStrong: themeConfig.theme.colors.primary[600],
-                }}
-              />
-            </Box>
-            {/* Formulário */}
-  
-  
-            {/* Photo Area */}
+              styleSheet={{'text-align':'center'}}
+              fullWidth
+              textFieldColors={{
+                neutral: {
+                  textColor: themeConfig.theme.colors.neutrals[200],
+                  mainColor: themeConfig.theme.colors.neutrals[900],
+                  mainColorHighlight: themeConfig.theme.colors.primary[500],
+                  backgroundColor: themeConfig.theme.colors.neutrals[800],
+                },
+              }}
+            />
+
+            <Button
+              type='submit'
+              label='Entrar'
+              fullWidth
+              buttonColors={{
+                contrastColor: themeConfig.theme.colors.neutrals["000"],
+                mainColor: themeConfig.theme.colors.primary[500],
+                mainColorLight: themeConfig.theme.colors.primary[400],
+                mainColorStrong: themeConfig.theme.colors.primary[600],
+              }}
+              disabled={submitDisabled}
+            />
+          </Box>
+          {/* Formulário */}
+
+
+          {/* Photo Area */}
+          <Box
+            styleSheet={{
+              display: submitDisabled ? 'none' : 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              maxWidth: '300px',
+              padding: '16px',
+              backgroundColor: themeConfig.theme.colors.neutrals[800],
+              border: '1px solid',
+              borderColor: themeConfig.theme.colors.neutrals[999],
+              borderRadius: '10px',
+              flex: 1,
+              minHeight: '240px',
+            }}
+          >
             <Box
               styleSheet={{
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
-                maxWidth: '200px',
-                padding: '16px',
-                /*backgroundColor: themeConfig.theme.colors.neutrals[800],
+                maxWidth: '100%',
+                backgroundColor: themeConfig.theme.colors.neutrals[100],
                 border: '1px solid',
                 borderColor: themeConfig.theme.colors.neutrals[999],
-                borderRadius: '10px',*/
-                flex: 1,
-                minHeight: '240px',
+                borderRadius: '10px',
+                flex: 1
+              }}
+            >
+              <Text
+                variant="body4"
+                styleSheet={{
+                  color: themeConfig.theme.colors.neutrals[200],
+                  backgroundColor: themeConfig.theme.colors.neutrals[900],
+                  padding: '3px 10px',
+                  borderRadius: '1000px'
+                }}
+              >
+                {username}
+              </Text>
+              <Image
+                styleSheet={{
+                  maxWidth: '70%',
+                  borderRadius: '50%',
+                  marginBottom: '16px',
+                }}
+                src={`https://github.com/${username}.png`}
+              />
+            </Box>
+            <Box
+              styleSheet={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                maxWidth: '100%',
+                backgroundColor: themeConfig.theme.colors.neutrals[500],
+                border: '1px solid',
+                borderColor: themeConfig.theme.colors.neutrals[999],
+                borderRadius: '10px',
+                flex: 1
               }}
             >
               <Image
                 styleSheet={{
+                  width: '80px',
                   borderRadius: '50%',
-                  marginBottom: '16px',
+                  marginTop: '16px',
                 }}
                 src={`https://github.com/${username}.png`}
               />
@@ -150,9 +197,10 @@ export default function HomePage() {
                 {username}
               </Text>
             </Box>
-            {/* Photo Area */}
           </Box>
+          {/* Photo Area */}
         </Box>
-      </>
-    );
-  }
+      </Box>
+    </>
+  );
+}
